e989b6acfcb70497c0d90e1247c48a63ddb4ec28
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2023-01-10 09:26:56 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+using static System.Reflection.Metadata.BlobBuilder;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        PosibilityController posibilityController = new PosibilityController(10);
+        posibilityController.SetProbabilities(new float[] { 0.5f, 0.25f, 0.2f,0.05f });
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine(posibilityController.GetRandomValue());
+        }
+        Console.ReadLine();
+    }
+    public class PosibilityController
+    {
+        private int Length = 0;
+        public float[] Probabilities { get; private set; }
+        public PosibilityController(int length)
+        {
+            Length = length;
+            Probabilities = new float[Length];
+        }
+        public void SetProbabilities(float[] probabilities)
+        {
+            if (probabilities.Sum() > 1) throw new Exception("Probability can't be more than 1");
+            if (probabilities.Sum() < 1) throw new Exception("Probability can't be less than 1");
+            Probabilities = probabilities;
+        }
+        public float GetRandomValue()
+        {
+            float total = 0;
+            foreach (float elem in Probabilities)
+            {
+                total += elem;
+            }
+            float randomPoint = Convert.ToSingle(new Random().NextDouble()) * total;
+            for (int i = 0; i < Probabilities.Length; i++)
+            {
+                if (randomPoint < Probabilities[i])
+                {
+                    return i;
+                }
+                else
+                {
+                    randomPoint -= Probabilities[i];
+                }
+            }
+            return Probabilities.Length - 1;
+        }
+
+    }
+}
+
